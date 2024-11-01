0c75d93dac4e5138bd7bbb019fbdbcf9c094049a
Aryan Mane <aryanmane7916@gmail.com>
2024-11-01 13:17:10 +0530

    feat: stats card

--- src/app/(dashboard)/_components/Overview.tsx
+++ src/app/(dashboard)/_components/Overview.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react"
 import { DateRangePicker } from "@/components/ui/date-range-picker"
 import { MAX_DATE_RANGE_DAYS } from "@/lib/constant"
 import { toast } from "sonner"
+import StatsCards from "./StatsCard"
 
 export default function Overview({ userSettings }: { userSettings: UserSettings }) {
     const [dateRange, setDateRange] = useState<{from: Date; to: Date}>({
@@ -14,24 +15,33 @@ export default function Overview({ userSettings }: { userSettings: UserSettings 
     })
 
     return (
-        <div className="flex flex-wrap items-end justify-between gap-8 py-4 px-12">
-            <h2 className="text-2xl font-bold">Overview</h2>
-            <div className="flex items-center">
-                <DateRangePicker
-                    initialDateFrom={dateRange.from}
-                    initialDateTo={dateRange.to}
-                    showCompare={false} 
-                    onUpdate={(values) => {
-                        const { from, to } = values.range
+        <>
+            <div className="flex flex-wrap items-end justify-between gap-8 py-4 px-12">
+                <h2 className="text-2xl font-bold">Overview</h2>
+                <div className="flex items-center">
+                    <DateRangePicker
+                        initialDateFrom={dateRange.from}
+                        initialDateTo={dateRange.to}
+                        showCompare={false} 
+                        onUpdate={(values) => {
+                            const { from, to } = values.range
 
-                        if (!from || !to) return
-                        if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
-                            toast.error(`The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days`)
-                            return
-                        }
-                        setDateRange({from, to})
-                    }}/>
+                            if (!from || !to) return
+                            if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                                toast.error(`The selected date range is too big. Max allowed range is ${MAX_DATE_RANGE_DAYS} days`)
+                                return
+                            }
+                            setDateRange({from, to})
+                        }}/>
+                </div>
             </div>
-        </div>
+            <div className="flex flex-col mx-12 gap-2">
+                <StatsCards
+                    userSettings={ userSettings }
+                    from={ dateRange.from }
+                    to={ dateRange.to }
+                />
+            </div>
+        </>
     )
 }
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,4 +1,6 @@
 
+import { Currencies } from "./currency"
+
 export function DateToUTCDate(date: Date) {
     return new Date(
         Date.UTC(
@@ -11,4 +13,13 @@ export function DateToUTCDate(date: Date) {
             date.getMilliseconds()
         )
     )
+}
+
+export function GetFormatterForCurrency(currency: string) {
+    const locale = Currencies.find(c => c.value === currency)?.locale
+
+    return new Intl.NumberFormat(locale, {
+        style: "currency",
+        currency
+    })
 }
